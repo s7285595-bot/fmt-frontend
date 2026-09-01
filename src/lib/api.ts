c8587a0bbea3b2fdb410151c331f709a1923c408
Sai Ram Ponnala <sai ram>
2026-09-02 04:21:22 +0530

    sixth commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 
-const API_BASE_URL = "http://localhost:8080/api";
-
+// const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
 /* =========================
    REGISTER
 ========================= */
